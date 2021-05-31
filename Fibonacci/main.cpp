e535bcff8286daa79f9d22a8fd7649340afb3903
Michael Van Der Poll <michaelvdp@gmail.com>
2021-05-31 16:12:08 +0200

    Fibonacci exercise

--- Fibonacci/main.cpp
+++ Fibonacci/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 
+using namespace std;
+
+int Fibonacci(int);
+int Fib(int num);
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Fibonacci!" << endl;
+    cout << "Enter a number to Fibonacci it:";
+    int input;
+    cin >> input;
+    cout << "Answer: " << Fibonacci(input) << " solved by Recursion"<<endl;
+    cout << "Answer: " << Fib(input) << " solved by Iteration" << endl;
     return 0;
+}
+
+int Fibonacci(int value) {
+    if(value < 2) return value;
+    return Fibonacci(value - 1) + Fibonacci(value - 2);
+}
+
+int Fib(int value) {
+    int x = 0, y = 1;
+    int tmp = 0;
+
+    for (int i = 0; i < value; i++) {
+        tmp = x + y;
+        x = y;
+        y = tmp;
+    }
+    return x;
 }
